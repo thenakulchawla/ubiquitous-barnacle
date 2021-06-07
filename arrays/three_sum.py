a7faee3e3a3f629cd37ff6fd02b7f7088b37adfb
thenakulchawla <msg@nakul.net>
2021-06-07 00:27:33 -0700

    Remove infinite loop

--- arrays/three_sum.py
+++ arrays/three_sum.py
@@ -4,25 +4,27 @@ def three_sum(nums):
     if len(nums) < 3:
         return []
 
-    num_sorted = sorted(nums)
+    nums_sorted = sorted(nums)
     list_to_return = []
 
-    for i in range(len(num_sorted) - 2):
-        if i > 0 and nums[i] == nums[i-1]:
+    for i in range(len(nums_sorted) - 2):
+        if i != 0 and (nums_sorted[i] == nums_sorted[i-1]):
             continue
         j = i+1
-        k = len(nums) - 1
-        while j != k:
-            if k < len(nums) - 1 and nums[k] == nums[k+1]:
+        k = len(nums_sorted) - 1
+        while j < k:
+            if k < len(nums_sorted) - 1 and nums_sorted[k] == nums_sorted[k+1]:
                 k -= 1
 
-            if nums[i] + nums[j] + nums[k] == 0:
-                temp = [nums[i], nums[j], nums[k]]
+            if nums_sorted[i] + nums_sorted[j] + nums_sorted[k] == 0:
+                temp = [nums_sorted[i], nums_sorted[j], nums_sorted[k]]
                 list_to_return.append(temp)
-
-            elif nums[i] + nums[j] + nums[k] > 0:
+                j += 1
                 k -= 1
-            elif nums[i] + nums[j] + nums[k] < 0:
+
+            elif nums_sorted[i] + nums_sorted[j] + nums_sorted[k] > 0:
+                k -= 1
+            elif nums_sorted[i] + nums_sorted[j] + nums_sorted[k] < 0:
                 j += 1
     return list_to_return
 
