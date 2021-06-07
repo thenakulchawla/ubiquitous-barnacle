3c3b9ad5699777617d39985b685f3a4ebeb21062
thenakulchawla <msg@nakul.net>
2021-06-07 00:45:21 -0700

    Correct colution

--- arrays/three_sum.py
+++ arrays/three_sum.py
@@ -8,19 +8,22 @@ def three_sum(nums):
     list_to_return = []
 
     for i in range(len(nums_sorted) - 2):
+        if nums_sorted[i] > 0:
+            break
         if i != 0 and (nums_sorted[i] == nums_sorted[i-1]):
             continue
+
         j = i+1
         k = len(nums_sorted) - 1
         while j < k:
-            if k < len(nums_sorted) - 1 and nums_sorted[k] == nums_sorted[k+1]:
-                k -= 1
 
             if nums_sorted[i] + nums_sorted[j] + nums_sorted[k] == 0:
                 temp = [nums_sorted[i], nums_sorted[j], nums_sorted[k]]
                 list_to_return.append(temp)
                 j += 1
                 k -= 1
+                while j < k and nums_sorted[j] == nums_sorted[j-1]:
+                    j += 1
 
             elif nums_sorted[i] + nums_sorted[j] + nums_sorted[k] > 0:
                 k -= 1
@@ -30,6 +33,6 @@ def three_sum(nums):
 
 
 if __name__ == "__main__":
-    nums = [-1, 0, 1, 2, -1, -4]
+    nums = [-2, 0, 0, 2, 2]
     nums1 = three_sum(nums)
     print(nums1)
